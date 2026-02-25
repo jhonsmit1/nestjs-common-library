dcfb9e99d5a4d65908172eacd67fabab0c890943
jhonsmit1 <realtube565@gmail.com>
2026-02-25 17:34:13 -0500

    exception filter add

---
+++ dist/logger/logger.service.d.ts
@@ -0,0 +1,15 @@
+import { ILoggerAdapter } from "./interfaces/logger-adapter.interface";
+import { LoggerModuleOptions } from "./interfaces/logger-options.interface";
+import { LogLevel } from "./interfaces/log-level.type";
+export declare class LoggerService {
+    private readonly logger;
+    private readonly options;
+    constructor(logger: ILoggerAdapter, options: LoggerModuleOptions);
+    private enrichMeta;
+    log(level: LogLevel, message: string, meta?: any): void;
+    info(message: string, meta?: any): void;
+    warn(message: string, meta?: any): void;
+    debug(message: string, meta?: any): void;
+    verbose(message: string, meta?: any): void;
+    error(message: string, trace?: string | Error, meta?: any): void;
+}
